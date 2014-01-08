b7cb7eea467c27c7ab0eda835dc7c0230fe89c28
Ken Youens-Clark <kclark@cshl.edu>
2014-01-08 18:58:33 -0500

    customizations for 40

--- gramene/modules/EnsEMBL/Web/Document/Element/FooterLinks.pm
+++ gramene/modules/EnsEMBL/Web/Document/Element/FooterLinks.pm
@@ -7,17 +7,14 @@ use strict;
 sub content {
 
   return qq(
-    <script type="text/javascript">
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', 'UA-1624628-5']);
-    _gaq.push(['_trackPageview']);
-
-    (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    })();
+      ga('create', 'UA-1624628-10', 'gramene.org');
+      ga('send', 'pageview');
 
     </script>
     <div class="twocol-right right unpadded">
