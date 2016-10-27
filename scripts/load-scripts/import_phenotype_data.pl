159be6d476bae7d39c8c2c778ccf27a0ad7a01cd
sharon <weix@cshl>
2016-10-27 14:54:44 -0400

    add qtaro data

--- scripts/load-scripts/import_phenotype_data.pl
+++ scripts/load-scripts/import_phenotype_data.pl
@@ -2673,6 +2673,7 @@ sub href_value{
 	my $ret;
 	if( $value =~ /,/){ #RM227,RM555,
 		my @tmp_markers = split ',',$value;
+		print "$tmp_markers[0]\n";
 		$ret = join ',', (map{my $tmp=$href_tmpl; warn ("tmp=$tmp\n"); $tmp =~ s/\$\$\$/$value/g; warn ("tmp=$tmp\n"); $tmp} @tmp_markers);
 		
 	}else{
