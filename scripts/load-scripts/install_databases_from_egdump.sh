8cc27df1b488e447cebe80a8595e9307ce470f77
weix-cshl <weix@cshl.edu>
2018-06-01 14:25:00 -0400

    add new scripts and files

--- scripts/load-scripts/install_databases_from_egdump.sh
+++ scripts/load-scripts/install_databases_from_egdump.sh
@@ -33,7 +33,7 @@ fi
 echo "change directory to $DBDIR"
 cd $DBDIR
 
-mysqlparam=" -u weix -pwarelab -h $DBHOST "
+mysqlparam=" -u $USER -p$DBPASSWD -h $DBHOST "
 echo "mysqlparam is $mysqlparam"
 
 
@@ -42,16 +42,16 @@ for k in core funcgen variation otherfeature compara mart
 	ls -d *$k* > ../Epl$k
 done
 
-#for k in core funcgen variation otherfeature compara mart 
-for k in funcgen variation otherfeature compara mart 
+for k in core funcgen variation otherfeature compara mart 
+#for k in mart 
         do echo Epl$k 
 	for db in `cat ../Epl$k `
 		do echo $db  >> ../Epl$k.log
-		/usr/local/mysql/bin/mysql $mysqlparam -e "drop database if exists $db; create database $db" 
-		cd $db 
-		gunzip -c ${db}.sql.gz | /usr/local/mysql/bin/mysql $mysqlparam $db
-		gunzip *.txt.gz 
-		ls *.txt | xargs /usr/local/mysql/bin/mysqlimport $mysqlparam -L -l $db
-		cd ../ 
+		/usr/local/mysql/bin/mysql $mysqlparam -e "drop database if exists $db; create database $db"  2>&1 >> ../Epl$k.log 
+		cd $db 2>&1 >> ../../Epl$k.log
+		gunzip -c ${db}.sql.gz | /usr/local/mysql/bin/mysql $mysqlparam $db 2>&1 >> ../../Epl$k.log
+		gunzip *.txt.gz 2>&1 >> ../../Epl$k.log
+		ls *.txt | xargs /usr/local/mysql/bin/mysqlimport $mysqlparam -L -l $db 2>&1 >> ../../Epl$k.log
+		cd ../ 2>&1 >> ../../Epl$k.log
 	done
 done
---
+++ scripts/load-scripts/install_pan_databases_from_dump.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+
+DBDIR=$1
+DBHOST=$2
+
+if [[ -z $DBDIR ]]
+then
+
+	echo "Need working directory."
+        exit
+fi
+
+if [[ -z $DBHOST ]]
+then
+
+        echo "Need DBHOST to install databases on."
+        exit
+fi
+
+echo "the working directory is $DBDIR, DBHOST is $DBHOST, continue? [Y/N]"
+
+read reply
+
+echo "reply is $reply"
+
+if [[  $reply != [Yy] ]]
+then 
+	echo "Bye"
+	exit
+fi
+
+echo "change directory to $DBDIR"
+cd $DBDIR
+
+mysqlparam=" -u $USER -p$DBPASSWD -h $DBHOST "
+echo "mysqlparam is $mysqlparam"
+
+
+	for db in `ls `
+		do echo $db  >> ../panDB.log
+		/usr/local/mysql/bin/mysql $mysqlparam -e "drop database if exists $db; create database $db"  2>&1 >> ../panDB.log 
+		cd $db 2>&1 >> ../../panDB.log
+		gunzip -c *.sql.gz | /usr/local/mysql/bin/mysql $mysqlparam $db 2>&1 >> ../../panDB.log
+		gunzip *.txt.gz 2>&1 >> ../../panDB.log
+		ls *.txt | xargs /usr/local/mysql/bin/mysqlimport $mysqlparam -L -l $db 2>&1 >> ../../panDB.log
+		cd ../ 2>&1 >> ../../panDB.log
+	done
