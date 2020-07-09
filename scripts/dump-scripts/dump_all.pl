2006cd0726a965f8328c5f7f30fcd63b09a1af72
Andrew Olson <olson@cshl.edu>
2020-07-09 15:02:59 -0400

    allow multiple logic names and only output cdna for biotype ne 'protein_coding'

--- scripts/dump-scripts/dump_all.pl
+++ scripts/dump-scripts/dump_all.pl
@@ -50,7 +50,7 @@ dump_transcripts.pl  [options]
 =cut
 
 my ($species, $registry, $coding, $longest);
-my (%exclude_gene,%exclude_analysispgm,%analysispgm,%exclude_clone,$bylogicname,$outfile);
+my (%exclude_gene,%exclude_analysispgm,%analysispgm,%exclude_clone,@logicNames,$outfile);
 my $margin=undef;
 {  #Argument Processing
   my $help=0;
@@ -60,7 +60,7 @@ my $margin=undef;
   my @analysispgm=();
   my @exclude_clone=();
   GetOptions( "help|?"=>\$help,"man"=>\$man
-	      ,"bylogicname=s"=>\$bylogicname
+	      ,"bylogicname=s"=>\@logicNames
 	      ,"species=s"=>\$species
 	      ,"registry=s"=>\$registry
 	      ,"outfile=s"=> \$outfile
@@ -82,7 +82,16 @@ my $slice_adaptor = $ENS_DBA->get_SliceAdaptor;
 #my $assembly_type=$ENS_DBA->get_MetaContainer()->get_default_assembly();
 
 my @genes=@ARGV;
-@genes or  @genes=@{$gene_adaptor->fetch_all_by_logic_name($bylogicname)};
+if (not @genes) {
+  @genes=();
+  for my $logicName (@logicNames) {
+    my $gs = $gene_adaptor->fetch_all_by_logic_name($logicName);
+    for my $g (@$gs) {
+      push @genes, $g
+    }
+  }
+}
+# @genes or  @genes=@{$gene_adaptor->fetch_all_by_logic_name($bylogicname)};
 print "retrieved gene count", scalar @genes;
 
 #exit;
@@ -113,71 +122,68 @@ if ($outfile) {
 
 
 foreach my $gene (@genes) {
-  #print "! $geneid\n";
-  
-  $count{total_genes}++;
-  if(@ARGV) {    # Stable ids GRMGnnnnnnn
-	my $gid=$gene;
-    eval { $gene=$gene_adaptor->fetch_by_stable_id($gid) };
-    print STDERR "$@\n" and next if $@;
-  } #else {	#internal ids
-  #  eval { $gene= $gene_adaptor->fetch_by_dbID($geneid); };
-  #  print STDERR "gene_id $geneid:\n$@\n" and next if $@;
-  #  # fails e.g. if gene has no exons
-  #}
-  
-  next unless $gene;
-  
-  #my ($working_set_attrib) = @{$gene->get_all_Attributes('working-set')};
-  #print "gene attrib is ", $working_set_attrib->code, "\n";
-  #next if ($species =~ /zea|mays|maize/i && !$working_set_attrib);
-  
-  
-  
-  $count{qualified_genes}++;
-  
-  my @transcripts;
-  
-    eval {@transcripts = @{$gene->get_all_Transcripts}};
+
+    #print "! $geneid\n";
+
+    $count{total_genes}++;
+    if (@ARGV) {    # Stable ids GRMGnnnnnnn
+        my $gid = $gene;
+        eval { $gene = $gene_adaptor->fetch_by_stable_id($gid) };
+        print STDERR "$@\n" and next if $@;
+    }    #else {	#internal ids
+         #  eval { $gene= $gene_adaptor->fetch_by_dbID($geneid); };
+         #  print STDERR "gene_id $geneid:\n$@\n" and next if $@;
+         #  # fails e.g. if gene has no exons
+         #}
+
+    next unless $gene;
+
+    #my ($working_set_attrib) = @{$gene->get_all_Attributes('working-set')};
+    #print "gene attrib is ", $working_set_attrib->code, "\n";
+    #next if ($species =~ /zea|mays|maize/i && !$working_set_attrib);
+
+    $count{qualified_genes}++;
+
+    my @transcripts;
+
+    eval { @transcripts = @{ $gene->get_all_Transcripts } };
     print STDERR "$@" && next if $@;
-  
-  foreach my $trans (@transcripts) {
-  
-    #print join "\t", ($trans->stable_id, $trans->spliced_seq, "\n");
-    my $id = $trans->stable_id;
-    
-	my $aa_obj = $trans->translate;
-    
 
-    eval {$seqio_pr->write_seq($aa_obj)};
-    print STDERR "Cannot write seq for $id, $@" && next if $@;
+    foreach my $trans (@transcripts) {
 
+        #print join "\t", ($trans->stable_id, $trans->spliced_seq, "\n");
+        my $id = $trans->stable_id;
 
-	my $cdna_seq=$trans->spliced_seq;
+        my $cdna_seq = $trans->spliced_seq;
 
-      my $cdna_coding_start = $trans->cdna_coding_start;
-      my $cdna_coding_end   = $trans->cdna_coding_end;
-      my $seq_obj_cds = Bio::Seq->new(
-                                      -display_id => $id,
-                                      -seq => substr($cdna_seq,
-                                                     $cdna_coding_start-1,
-                                                     $cdna_coding_end-$cdna_coding_start+1)
-                                     );
+        my $seq_obj_cdna = Bio::Seq->new(
+            -display_id => $id,
+            -seq        => $cdna_seq,
+        );
 
-      $seqio_cds->write_seq($seq_obj_cds);
+        $seqio_cdna->write_seq($seq_obj_cdna);
+        if ( $trans->biotype eq 'protein_coding' ) {
+            my $aa_obj = $trans->translate;
 
+            eval { $seqio_pr->write_seq($aa_obj) };
+            print STDERR "Cannot write seq for $id, $@" && next if $@;
 
+            my $cdna_coding_start = $trans->cdna_coding_start;
+            my $cdna_coding_end   = $trans->cdna_coding_end;
+            my $seq_obj_cds       = Bio::Seq->new(
+                -display_id => $id,
+                -seq        => substr(
+                    $cdna_seq,
+                    $cdna_coding_start - 1,
+                    $cdna_coding_end - $cdna_coding_start + 1
+                )
+            );
 
-      my $seq_obj_cdna = Bio::Seq->new(
-                                    -display_id => $id,
-                                    -seq => $cdna_seq,
-                                   );
+            $seqio_cds->write_seq($seq_obj_cds);
+        }
 
-   
-      $seqio_cdna->write_seq($seq_obj_cdna);
-      
-	$count{qualified_transcripts}++;
-  }
+        $count{qualified_transcripts}++;
+    }
 
 
     
