d3a9cd65b42e308c3addc7feccef19ae4b5de2cf
weix-cshl <weix@cshl.edu>
2021-11-03 15:15:11 -0400

    commit changes

--- scripts/misc-scripts/get_split_genes.sql
+++ scripts/misc-scripts/get_split_genes.sql
@@ -1,1 +1,1 @@
-select genome_db.name, seq_member.stable_id from split_genes join seq_member using(seq_member_id) join genome_db using(genome_db_id);
+select genome_db.name as SPECIES, seq_member.stable_id as SPLIT_GENE_TRANSCRIPT from split_genes join seq_member using(seq_member_id) join genome_db using(genome_db_id) order by 1,2;
